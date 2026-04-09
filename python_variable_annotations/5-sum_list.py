f72db044e9bd9d3b13540c3e401be42f020d9952
Fooxii <ironata07@gmail.com>
2026-04-09 19:36:46 -0400

    Added script that uses type annotations to make sure it takes a parameter of a list of floats and returns a float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module 5-sum_list.py documentation"""
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Function documentation"""
+    return sum(input_list)
