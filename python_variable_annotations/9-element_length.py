281d63a483d82a6a95009c96ebea3eb6d4d70286
Fooxii <ironata07@gmail.com>
2026-04-12 14:09:49 -0400

    Fixed the function to fit what the checker expects

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """Module 9-element_length.py documentation"""
-from typing import Iterable, Sequence, Tuple, List, TypeVar
+from typing import Iterable, Sequence, Tuple, List
 
-T = TypeVar('T', bound=Sequence)
 
-def element_length(lst: Iterable[T]) -> List[Tuple[T, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Function documentation"""
     return [(i, len(i)) for i in lst]
