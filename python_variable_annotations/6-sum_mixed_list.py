57355e4c5f007598d8b622f2687e4fd8aacc70a0
Fooxii <ironata07@gmail.com>
2026-04-12 10:23:20 -0400

    Added a script that uses type annotations to make sure the function only takes a parameter of a list of integers or floats and returns the sum of that list as a float

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module 6-sum_mixed_list.py documentation"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_list: List[Union[int, float]]) -> float:
+    """Function documentation"""
+    return float(sum(mxd_list))
