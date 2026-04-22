fe28f9375357d86d6375bf8ecbd8c28bad18bd6f
Fooxii <ironata07@gmail.com>
2026-04-22 09:03:44 -0400

    pycodestyle fixes

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """Module 0-simple_helper_function documentation"""
+
+
 def index_range(page: int, page_size: int) -> tuple:
     """Function documentation"""
     start = (page - 1) * page_size
