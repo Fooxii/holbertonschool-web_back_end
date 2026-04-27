e2a8e9a889efe425b0feccb4f70de6cbfee6a91e
Fooxii <ironata07@gmail.com>
2026-04-27 14:03:18 -0400

    Added python function that changes all topics of a school document based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module 10-update_topics.py documentation"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function documentation"""
+    mongo_collection.update_many({"name": name}, {"$set": {topics}})
