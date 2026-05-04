7668bccca842e69a75e7d3b16d55508ad5568f87
Fooxii <ironata07@gmail.com>
2026-05-04 13:38:39 -0400

    Fixed indentation on the class HolbertonClass

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,16 +1,16 @@
 export class HolbertonClass {
-    constructor(year, location) {
-        this._year = year;
-        this._location = location;
-    }
+  constructor(year, location) {
+    this._year = year;
+    this._location = location;
+  }
     
-    get year() {
-        return this._year;
-    }
+  get year() {
+    return this._year;
+  }
     
-    get location() {
-        return this._location;
-    }
+  get location() {
+    return this._location;
+  }
 }
 
 const class2019 = new HolbertonClass(2019, 'San Francisco');
