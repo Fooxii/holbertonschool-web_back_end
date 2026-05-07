0e1d2b36a22bf11098b8bf7ccb7ea42e2c9da245
Fooxii <ironata07@gmail.com>
2026-05-07 15:47:30 -0400

    Fix type check in cleanSet function to ensure startString is a string and not empty

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   const result = [];
 
-  if (!startString) {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
 
