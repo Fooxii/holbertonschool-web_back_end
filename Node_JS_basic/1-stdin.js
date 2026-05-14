f421763d592c3eb3564fc69e977aa9c8a2055354
Fooxii <ironata07@gmail.com>
2026-05-14 15:12:47 -0400

    Fixed the encoding to be utf-8

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("UTF-8");
+process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (input) => {
   console.log(`Your name is: ${input.trim()}`);
 });
