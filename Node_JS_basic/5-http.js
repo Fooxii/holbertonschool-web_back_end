7142834246c3ba160451e00c33ce8615be1c98ff
Fooxii <ironata07@gmail.com>
2026-05-15 20:04:32 -0400

    Forgot to add the countStudents function from another file

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,4 +1,6 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
+const database = process.argv[2];
 
 const app = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/plain');
